b82d8eb03c57897cba0a6c4f52bf93cc4b0c4355
n-sudo-mac <nsadnote22@gmail.com>
2025-07-24 03:34:47 +0900

    create next-ts-tailwind template

--- src/components/articles/index.ts
+++
@@ -1,5 +0,0 @@
-// src/components/articles/index.ts
-export { default as ContentCard } from './ContentCard';
-export { default as ContentList } from './ContentList';
-export { default as TagFilter } from './TagFilter';
-export { default as RelatedArticles } from './RelatedArticles';
